e83da472ba7e011633192b8ddd8d140ebe773edd
Maria Regina Nula Lewar <marialewar36@gmail.com>
2025-12-10 16:47:43 +0700

    isi config

---
+++ config.py
@@ -0,0 +1,42 @@
+
+from config import ACADEMIC_CODES, ACTIVITY_CODES, SKILL_LIST
+# config.py (versi lengkap & konsisten)
+
+ACADEMIC_CODES = {
+    "Ilmu Komputer": 1,
+    "Manajemen": 2,
+    "Psikologi": 3,
+    "Ilmu Komunikasi": 4,
+    "Ekonomi": 5,
+    "Teknik Sipil": 6,
+    "Biologi": 7,
+    "Fisika": 8,
+    "Kimia": 9,
+    "Sastra Inggris": 10
+}
+
+ACTIVITY_CODES = {
+    "Debat": 1,
+    "PMR": 2,
+    "OSIS": 3,
+    "Pramuka": 4,
+    "Seni Musik": 5,
+    "Seni Rupa": 6,
+    "Olahraga": 7,
+    "Robotik": 8,
+    "Jurnalistik": 9,
+    "Teater": 10
+}
+
+SKILL_LIST = [
+    "Publik Speaking",
+    "Analisis Data",
+    "Menulis",
+    "Leadership",
+    "Desain",
+    "Negosiasi",
+    "Kolaborasi",
+    "Kreativitas",
+    "Problem Solving",
+    "Ketekunan"
+]
